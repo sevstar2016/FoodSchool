55e7f6aef77d3e82be54ebb27147cd68db88ef1f
sevka <sevstar2206@gmail.com>
2025-08-29 15:34:57 +0500

    fixes

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.api import auth as auth_router
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(title="FoodAPI", version="0.1.0", root_path="/api")
+    app = FastAPI(title="FoodAPI", version="0.1.0", docs_url="/api/docs", redoc_url="/api/redoc", openapi_url="/api/openapi.json", root_path="/api")
 
     @app.get("/health")
     def healthcheck():
